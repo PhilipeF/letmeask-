e26d85ccd19ec031eae05ebc8f620f301c0a7b91
Philipe de Souza Ferreira <philipesferreiraa@gmail.com>
2021-06-28 20:54:07 -0300

    tela home styles

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import '../styles/button.scss';
+
+import { ButtonHTMLAttributes } from 'react';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLElement>;
+
+export function Button(props: ButtonProps) {
+  return <button className='button' {...props} />;
+}
+
+
