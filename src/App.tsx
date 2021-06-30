23536bd61ce3f238e3976f7e579357156eaae337
Philipe de Souza Ferreira <philipesferreiraa@gmail.com>
2021-06-30 18:39:59 -0300

    authentication firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-// import { Home } from './Pages/Home';
-import { NewRoom } from './Pages/NewRoom';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
-  return <NewRoom />;
+  return (
+    <BrowserRouter>
+      <Route path='/' exact component={Home} />
+      <Route path='/rooms/new' component={NewRoom} />
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/Pages/NewRoom.tsx
+++ src/Pages/NewRoom.tsx
@@ -1,8 +1,10 @@
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
+import { Link } from 'react-router-dom';
+
+import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
-import { Button } from '../components/Button';
 
 export function NewRoom() {
   return (
@@ -24,7 +26,7 @@ export function NewRoom() {
             <Button type='submit'>Criar sala</Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href='#'>Clique aqui</a>
+            Quer entrar em uma sala existente? <Link to='/'>Clique aqui</Link>
           </p>
         </div>
       </main>
