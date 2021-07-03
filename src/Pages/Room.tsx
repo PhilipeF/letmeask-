07cfd22d166fc7eec11a3fb6d71e927a4ac19d7d
Philipe de Souza Ferreira <philipesferreiraa@gmail.com>
2021-07-02 22:41:29 -0300

    create new question

--- src/Pages/Room.tsx
+++ src/Pages/Room.tsx
@@ -4,6 +4,9 @@ import { RoomCode } from '../components/RoomCode';
 import { useParams } from 'react-router-dom';
 
 import '../styles/room.scss';
+import { FormEvent, useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 type RoomParams = {
   id: string;
@@ -11,13 +14,43 @@ type RoomParams = {
 
 export function Room() {
   const params = useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState('');
+  const { user } = useAuth();
+
+  const roomId = params.id;
+
+  async function handlreSendQuestion(event: FormEvent) {
+    event.preventDefault(); //para ele não recarregar a tela
+
+    if (newQuestion.trim() === '') {
+      return;
+    }
+
+    if (!user) {
+      throw new Error('You must be logged in');
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user?.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false,
+    };
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+
+    setNewQuestion('');
+  }
 
   return (
     <div id='page-room'>
       <header>
         <div className='content'>
           <img src={logoImg} alt='Letmeask' />
-          <RoomCode code={params.id} />
+          <RoomCode code={roomId} />
         </div>
       </header>
       <main className='content'>
@@ -26,14 +59,28 @@ export function Room() {
           <span>4 perguntas</span>
         </div>
 
-        <form>
-          <textarea placeholder='O que você quer perguntar?'></textarea>
+        <form onSubmit={handlreSendQuestion}>
+          <textarea
+            placeholder='O que você quer perguntar?'
+            onChange={(event) => setNewQuestion(event.target.value)}
+            value={newQuestion}
+          />
           <div className='form-footer'>
+            {user ? (
+              <div className='user-info'>
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <p>sdsd</p>
+            )}
             <span>
               Para enviar um pergunta, <button>faça seu loguin</button>.
             </span>
           </div>
-          <Button type='submit'>Enviar pergunta</Button>
+          <Button type='submit' disabled={!user}>
+            Enviar pergunta
+          </Button>
         </form>
       </main>
     </div>
